c16cd2d6643f3e273b38147db6c0854cac8994bd
Jasmine Koh <jkoh0034@student.monash.edu>
2024-07-28 01:33:25 +0800

    Added client side router to access between home and details page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,20 @@
 import { useState } from 'react';
-import Posts from './Posts';
-import Navbar from './Navbar';
-import CategoryFilter from '../hooks/CategoryFilter';
+import Home from './Home';
+import Details from './Details';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 /**
  * The App component is the main component that renders everything.
  * @returns {JSX.Element} The App component.
  */
 function App() {
-
-  // Manages categories selected by user and passes it into Posts
-  const [selectedCategories, setSelectedCategories] = useState([]); 
-
   return (
-    <div>
-      <Navbar />
-      <CategoryFilter onCategoryChange={setSelectedCategories}/>
-      <Posts selectedCategories={selectedCategories}/>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/details" element={<Details />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Details.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function Details() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Details Page</h1>
+      {/* Add your details page content here */}
+    </div>
+  );
+}
+
+export default Details;
